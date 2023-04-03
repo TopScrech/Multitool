ca1e268a8e3874f5446600936e34f7e792a8ee39
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-04-03 17:26:51 +0200

    Initial Commit

---
+++ Multitool/MultitoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultitoolApp.swift
+//  Multitool
+//
+//  Created by Sergei Saliukov on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultitoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
